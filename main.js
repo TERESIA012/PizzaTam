25ae21dfda85f092bb42711ba3fa8b6ba44ff0a3
TERESIA012 <kingoriteresia@gmail.com>
2021-08-02 00:07:30 +0300

    add switch 1

--- main.js
+++ main.js
@@ -5,32 +5,151 @@
 //4.toppings=["Mushroom","Sausage","Bacon","Extra Cheese","Pineapple","Pepperoni"]
 
 //order display
-$(document).ready(function(){  
-    $("#btnn").click(function(){  
-        $(".customer").toggle();  
-    }); 
+$(document).ready(function () {
+    $("#btnn").click(function () {
+        $(".customer").toggle();
+    });
+    //append
+    let name = $("#pname option:selected").val();
+    console.log(name);
+    let size = $("#size option:selected").val();
+    let crust = $("#crust option:selected").val();
+    let toppings = $("#toppings option:selected").val();
+    let number = $("#number").val();
+    $(".p").append("Pizza :" + name + "<br>");
+    $(".s").append("Pizza Size:" + size + "<br>");
+    $(".t").append("Topping:" + crust + "<br>");
+    $(".c").append("Crust:" + toppings + "<br>");
+    $(".n").append("Number Of pizza:" + number + "<br>");
     //delivery details
-    $("#yes").click(function(){
+    $("#yes").click(function () {
         prompt("Enter location for delivery");
-        alert ("The delivery fee is 200.Your order has been received and is being processed.")
-      }); 
-});  
+        alert("The delivery fee is 200.Your order has been received and is being processed.")
+    });
+    flavourChange = function () {
+        console.log("Flavour changing")
+        let name = $("#pname option:selected").val();
+        console.log(name);
+        $(".p").html("Pizza :" + name + "<br>");
+
+    }
+    sizeChange = function () {
+        console.log("Size changing")
+        let size = $("#size option:selected").val();
+        console.log(size);
+        $(".s").html("Pizza Size:" + size + "<br>");
 
 
- 
+    }
+    crustChange = function () {
+        console.log("Crust changing")
+        let crust = $("#crust option:selected").val();
+        console.log(crust);
+        $(".c").html("Crust:" + crust + "<br>");
+
+
+    }
+    toppingsChange = function () {
+        console.log("Toppings changing")
+        let toppings = $("#toppings option:selected").val();
+        console.log(size);
+        $(".t").html("Topping:" + toppings + "<br>");
+
+
+    }
+    numberChange = function () {
+        console.log("Number changing")
+        let number = $("#number").val();
+        console.log(number);
+        $(".n").html("Number Of pizza:" + number + "<br>");
+    }
+    //constructor function order
+    let order = (name, size, crust, topping, number, total) => {
+        return { name, size, crust, topping, number, total };
+    };
+    //price
+    let price,totalBill;
+    switch (name) {
+        case name = "Cheese Pizza":
+            switch (size) {
+                case size = "Large":
+                    price = 1200
+                    if (crust === "Crispy") {
+                        totalBill = (price * number) + 350;
+                    } else if (crust === "Stuffed") {
+                        totalBill = (price * number) + 250;
+                    } else if (crust === "Gluten-free") {
+                        totalBill = (price * number) + 150;
+                    } else {
+                        totalBill = (price * number) + 280;
+                    }
+                    break;
+                case size = "Medium":
+                    price = 1200
+                    if (crust === "Crispy") {
+                        totalBill = (price * number) + 350;
+                    } else if (crust === "Stuffed") {
+                        totalBill = (price * number) + 250;
+                    } else if (crust === "Gluten-free") {
+                        totalBill = (price * number) + 150;
+                    } else {
+                        totalBill = (price * number) + 280;
+                    }
+                    break;
+                case size = "Small":
+                    price = 1200
+                    if (crust === "Crispy") {
+                        totalBill = (price * number) + 350;
+                    } else if (crust === "Stuffed") {
+                        totalBill = (price * number) + 250;
+                    } else if (crust === "Gluten-free") {
+                        totalBill = (price * number) + 150;
+                    } else {
+                        totalBill = (price * number) + 280;
+                    }
+                    break;
+                case size = "One Piecer":
+                    price = 1200
+                    if (crust === "Crispy") {
+                        totalBill = (price * number) + 350;
+                    } else if (crust === "Stuffed") {
+                        totalBill = (price * number) + 250;
+                    } else if (crust === "Gluten-free") {
+                        totalBill = (price * number) + 150;
+                    } else {
+                        totalBill = (price * number) + 280;
+                    }
+                    break;
+                case size = "Regular":
+                    price = 1200
+                    if (crust === "Crispy") {
+                        totalBill = (price * number) + 350;
+                    } else if (crust === "Stuffed") {
+                        totalBill = (price * number) + 250;
+                    } else if (crust === "Gluten-free") {
+                        totalBill = (price * number) + 150;
+                    } else {
+                        totalBill = (price * number) + 280;
+                    }
+                    break;
+                case size = "Pizza Kadogo":
+                    price = 1200
+                    if (crust === "Crispy") {
+                        totalBill = (price * number) + 350;
+                    } else if (crust === "Stuffed") {
+                        totalBill = (price * number) + 250;
+                    } else if (crust === "Gluten-free") {
+                        totalBill = (price * number) + 150;
+                    } else {
+                        totalBill = (price * number) + 280;
+                    }
+
+            }
+    }
 
 
 
-
-
-
-
-
-
-
-
-
-
+});
 
 
 
@@ -62,6 +181,8 @@ $(document).ready(function(){
 //     return number *(sizePrice + crustPrice + toppingsPrice)+deliveryFee
 // };
 // };
+
+
 
 
 
