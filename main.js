6d01cf7c96a1426d46a0e2f9d3110e502927c7a3
TERESIA012 <kingoriteresia@gmail.com>
2021-08-01 13:46:16 +0300

    create constructor function

--- main.js
+++ main.js
@@ -4,8 +4,19 @@
 //3.crust=["Crispy","Stuffed","Gluten-Free"]
 //4.toppings=["Mushroom","Sausage","Bacon","Extra Cheese","Pineapple","Pepperoni"]
 
+//constructor function
 
+function Bill(pname,size,crust,toppings,){
+this.pname=pname
+this.size=size
+this.crust=crust
+this.toppings=toppings
 
+Bill.prototype.fullBill=function(){
+    return this.pname + "of size "  + this.size +"with a crust of" + this.crust  + "and topping of" + this.toppings;
+};
+
+};
 
 
 
